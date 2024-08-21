3572e7c04bca9d9fee4ea91a1d8e9a1fd2c6ef63
Matthiasarther254 <matthiasarther@gmail.com>
2024-08-22 02:13:30 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,7 +22,7 @@ global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'Tech-God\n\nContact: +917466008456'
+global.author = 'Matthias_Arther\n\nContact: +254705774173'
 
 //console view/theme
 global.themeemoji = '🪀'
